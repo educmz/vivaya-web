c75f2b822a850619d80f9e77fd71d6f35c7e3c4b
Fabricio Fabian Quispe Barzola <ffaaliceht2003@gmail.com>
2026-09-04 01:02:04 -0500

    feat(home): move OrangeJourney below hero and speed up scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { OrangeHeroExperience } from "@/components/experiences/OrangeHeroExperience";
+import { OrangeJourney } from "@/components/home/OrangeJourney";
 import { OrangeStory } from "@/components/experiences/OrangeStory";
 import { ProductShowcase } from "@/components/experiences/ProductShowcase";
+
+import { HeroSection } from "@/components/home/HeroSection";
 import { FeaturedProducts } from "@/components/home/FeaturedProducts";
 import { BenefitsSection } from "@/components/home/BenefitsSection";
 import { PromotionsSection } from "@/components/home/PromotionsSection";
@@ -9,5 +11,20 @@ import { LocationsPreview } from "@/components/home/LocationsPreview";
 import { ClubPreview } from "@/components/home/ClubPreview";
 
 export default function Home() {
-  return <><OrangeHeroExperience /><FeaturedProducts /><OrangeStory /><BenefitsSection /><ProductShowcase /><PromotionsSection /><RappiBanner /><LocationsPreview /><ClubPreview /></>;
+  return (
+    <>
+      <HeroSection />
+
+      <OrangeJourney />
+
+      <FeaturedProducts />
+      <OrangeStory />
+      <BenefitsSection />
+      <ProductShowcase />
+      <PromotionsSection />
+      <RappiBanner />
+      <LocationsPreview />
+      <ClubPreview />
+    </>
+  );
 }
